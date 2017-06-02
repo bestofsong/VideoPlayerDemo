a4c8cee3edb30d253a28a2301997c893b55544ef
wansong <betterofsong@gmail.com>
2017-06-02 23:22:05 +0800

    Initial Commit

---
+++ testZKUIKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testZKUIKit
+//
+//  Created by wansong on 02/06/2017.
+//  Copyright © 2017 zhike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testZKUIKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testZKUIKit
+//
+//  Created by wansong on 02/06/2017.
+//  Copyright © 2017 zhike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
